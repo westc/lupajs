40c0e2f56bf750ecd077420f808a7fb318e128b6
Chris West <westc@live.com>
2023-12-15 14:03:49 -0400

    Separate lupa-iframe.js and handle this change in Gulpfile.js.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const babelCore = require('@babel/core');
+const terser = require('terser');
 const uglify = require('gulp-uglify');
 const replace = require('gulp-replace');
 const rename = require('gulp-rename');
@@ -10,41 +12,90 @@ const fs = require('fs');
 const cssSrc = 'src/lupa.css';
 const htmlSrc = 'src/lupa.html';
 const jsSrc = 'src/lupa.js';
+const jsIframeSrc = 'src/lupa-iframe.js';
 const dirDest = 'dist';
 const jsDest = `${dirDest}/lupa.js`;
 
-// Define a task to transpile and minify JavaScript
-function buildJs() {
-  // Read the content of the HTML file
-  const cssContent = new CleanCSS().minify(fs.readFileSync(cssSrc, 'utf8')).styles;
-  const htmlContent = htmlMinifier.minify(fs.readFileSync(htmlSrc, 'utf8'), {
-    collapseWhitespace: true,
-    removeComments: false,
-  });
+const babelOptions = {
+  // presets: ['@babel/preset-env']
+};
 
-  // Replace EXTERNAL_HTML_FILE with the HTML content
-  return gulp.src(jsSrc)
-    .pipe(replace('EXTERNAL_CSS_FILE_FROM_GULP', JSON.stringify(cssContent)))
-    .pipe(replace('EXTERNAL_HTML_FILE_FROM_GULP', JSON.stringify(htmlContent)))
-    .pipe(gulp.dest(dirDest));
+// Read the content of the HTML, CSS and IFRAME JS file
+const jsIframeContent = fs.readFileSync(jsIframeSrc, 'utf8');
+
+/** @type {{[key in ("CSS"|"HTML"|"JS_IFRAME"|"JS_IFRAME_MIN")]?: string}} */
+const PLACEHOLDER_VALUES = {};
+
+
+function getReplacer(useJsIframeMin=false) {
+  return replace(
+    /((?<=^|\r|\n)(?:(?!\r|\n)\s)*|)\[\[(\w+)_FILE_PLACEHOLDER\]\]/g,
+    (_, prefix, type) => {
+      const result = PLACEHOLDER_VALUES[(type === 'JS_IFRAME' && useJsIframeMin) ? 'JS_IFRAME_MIN' : type];
+      return prefix ? result.replace(/^/gm, prefix) : result;
+    }
+  );
 }
 
-gulp.task('build-js', buildJs);
-
-gulp.task('minify-js', gulp.series('build-js', function() {
-  return gulp.src(jsDest)
-    .pipe(babel())
-    .pipe(uglify({
-      compress: { unused: false },
-      mangle: false
-    }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(dirDest));
-}));
 
 // Watch for changes and run the build-js task
 gulp.task('watch', function () {
-  gulp.watch([cssSrc, htmlSrc, jsSrc], gulp.series('minify-js'));
+  gulp.watch(
+    [cssSrc, htmlSrc, jsSrc, jsIframeSrc],
+    gulp.series(
+
+      // Define the placeholder values.
+      async function definePlaceholderValues() {
+        if (Object.keys(PLACEHOLDER_VALUES).length) return;
+      
+        Object.assign(PLACEHOLDER_VALUES, {
+          CSS: JSON.stringify(new CleanCSS().minify(fs.readFileSync(cssSrc, 'utf8')).styles),
+          HTML: JSON.stringify(htmlMinifier.minify(fs.readFileSync(htmlSrc, 'utf8'), {
+            collapseWhitespace: true,
+            removeComments: false,
+          })),
+          JS_IFRAME: jsIframeContent,
+          JS_IFRAME_MIN: (await terser.minify(
+            babelCore.transformSync(jsIframeContent, babelOptions).code
+          )).code
+        });
+      },
+
+      // Define a task to transpile and minify JavaScript
+      async function buildMiniJS(done) {
+        // Read the content of the HTML file
+        const cssContent = new CleanCSS().minify(fs.readFileSync(cssSrc, 'utf8')).styles;
+        const htmlContent = htmlMinifier.minify(fs.readFileSync(htmlSrc, 'utf8'), {
+          collapseWhitespace: true,
+          removeComments: false,
+        });
+      
+        return gulp.src(jsSrc)
+          .pipe(getReplacer(true))
+          .pipe(gulp.dest(dirDest));
+      },
+
+      // Define a task to transpile and minify JavaScript
+      function buildJS() {
+        return gulp.src(jsSrc)
+          .pipe(getReplacer())
+          .pipe(rename({ suffix: '.full' }))
+          .pipe(gulp.dest(dirDest));
+      },
+
+      async function minifyJS() {
+        return gulp.src(jsDest)
+          .pipe(babel(babelOptions))
+          .pipe(uglify({
+            compress: { unused: false },
+            mangle: false
+          }))
+          .pipe(rename({ suffix: '.min' }))
+          .pipe(gulp.dest(dirDest));
+      }
+
+    )
+  );
 });
 
 // Default task: Run 'watch' as a default task
